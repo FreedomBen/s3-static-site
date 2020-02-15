1c365c291e9c27a07e000c6f3ede19dc55557b3b
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-02-14 19:28:20 -0700

    Move stuff to variables to DRY code

--- generate.sh
+++ generate.sh
@@ -2,18 +2,22 @@
 
 set -x
 
+SUBDOMAIN=staging
+DOMAIN=example
+TLD=com
+
 erb \
   region=us-west-2 \
   environment=staging \
-  site_sub_dom=staging \
-  site_domain=busyuntilbedtime \
-  site_tld=com \
+  site_sub_dom=${SUBDOMAIN} \
+  site_domain=${DOMAIN} \
+  site_tld=${TLD} \
   provision.tf.erb > staging-provision.tf
 
 erb \
   region=us-west-2 \
   environment=production \
-  site_sub_dom=www \
-  site_domain=busyuntilbedtime \
-  site_tld=com \
+  site_sub_dom=${SUBDOMAIN} \
+  site_domain=${DOMAIN} \
+  site_tld=${TLD} \
   provision.tf.erb > production-provision.tf
