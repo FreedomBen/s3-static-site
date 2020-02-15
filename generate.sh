c3fb2cff62499544547bbb1af6d9de9f929c4619
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-02-14 19:20:41 -0700

    Add geo restriction flag, fix some bugs
    Had to change variable names to lower case to support erb command line
    variables

--- generate.sh
+++ generate.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-erb provision.tf.erb > provision.tf
+set -x
+
+erb \
+  region=us-west-2 \
+  environment=staging \
+  site_sub_dom=staging \
+  site_domain=busyuntilbedtime \
+  site_tld=com \
+  provision.tf.erb > staging-provision.tf
+
+erb \
+  region=us-west-2 \
+  environment=production \
+  site_sub_dom=www \
+  site_domain=busyuntilbedtime \
+  site_tld=com \
+  provision.tf.erb > production-provision.tf
